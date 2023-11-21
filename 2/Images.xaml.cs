bbb5475a3dd832bcde4e876f692b406bb09b8b7b
Артур <@DESKTOP-0ISUU4Q>
2023-11-21 14:46:10 +0300

    Добавьте файлы проекта.

---
+++ 2/Images.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static _2.MainWindow;
+
+namespace _2
+{
+    /// <summary>
+    /// Логика взаимодействия для Images.xaml
+    /// </summary>
+    public partial class Images : UserControl
+    {
+        private double rotationAngle = 0;
+        public Images()
+        {
+            InitializeComponent();
+        }
+        private void WrapPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            textBlock1.Text = "https://kakogo-chisla.ru/wp-content/uploads/2022/06/Den-fotografii-prirody-2048x1363.jpg";
+            Zadanie2.indexImages++;
+        }
+
+        private void btnRight_Click(object sender, RoutedEventArgs e)
+        {
+            rotationAngle += 90;
+            ApplyRotation();
+        }
+
+        private void btnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            rotationAngle -= 90;
+            ApplyRotation();
+        }
+
+        private void ApplyRotation()
+        {
+            RotateTransform rotateTransform = new RotateTransform(rotationAngle);
+            image1.RenderTransform = rotateTransform;
+        }
+
+    }
+}
---
+++ 2/Zadanie2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _2
+{
+    /// <summary>
+    /// Логика взаимодействия для Zadanie2.xaml
+    /// </summary>
+    public partial class Zadanie2 : Window
+    {
+
+        public static int indexImages = 0;
+
+        public Zadanie2()
+        {
+            InitializeComponent();
+        }
+        private void NavigateToZadanie1(object sender, RoutedEventArgs e)
+        {
+            MainWindow zadanie1Window = new MainWindow();
+
+            this.Close();
+
+            zadanie1Window.Show();
+        }
+    }
+}
